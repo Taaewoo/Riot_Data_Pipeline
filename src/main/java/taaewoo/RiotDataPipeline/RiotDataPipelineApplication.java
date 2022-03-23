dbcc58868d8bf0ed38e16cc6e27aa7941249c226
Taewoo <kauasltw@gmail.com>
2022-03-24 02:12:33 +0900

    Adding Spring Boot project

---
+++ src/main/java/taaewoo/RiotDataPipeline/RiotDataPipelineApplication.java
@@ -0,0 +1,13 @@
+package taaewoo.RiotDataPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotDataPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotDataPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/taaewoo/RiotDataPipeline/RiotDataPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package taaewoo.RiotDataPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiotDataPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
