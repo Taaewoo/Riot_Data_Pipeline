5759781a2e6671c0b2089d2a64d32b46ae118e57
Taewoo <kauasltw@gmail.com>
2022-07-13 23:10:02 +0900

    Spark platform 추가 및 consumer pyspark 테스트 코드 작성

---
+++ platform/spark/spark_dir/consume_match_data.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+
+sc = SparkSession.builder.getOrCreate()
+
+sc.sparkContext.setLogLevel('ERROR')
+
+# Read stream
+log = sc.readStream.format("kafka") \
+.option("kafka.bootstrap.servers", "host.docker.internal:19096,host.docker.internal:29096,host.docker.internal:39096") \
+.option("subscribe", "summoner-match") \
+.option("startingOffsets", "earliest") \
+.load()
+
+# Write stream - console
+query = log.selectExpr("CAST(value AS STRING)") \
+.writeStream \
+.format("console") \
+.option("truncate", "false") \
+.start()
+
+query.awaitTermination()
